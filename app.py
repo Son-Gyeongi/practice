a9321763c75a424ae6be02b0c92e5c119ea5474a
Son Gyeong-i <thsruddl77@gmail.com>
2022-04-20 20:12:55 +0900

    포스팅 박스 댓글 박스 합쳤습니다.

--- app.py
+++ app.py
@@ -10,6 +10,33 @@ db = client.dbsparta
 def home():
    return render_template('index.html')
 
+#상우님 영균님이 한 요리 포스트 올리기 서버
+@app.route('/food', methods=['POST'])
+def food_post():
+    name_receive = request.form['name_give']
+    url_receive = request.form['url_give']
+    choice_receive = request.form['choice_give']
+    comment_receive = request.form['comment_give']
+
+    food_list = list(db.food.find({}, {'_id': False}))
+    count = len(food_list) + 1
+
+    doc = {
+        'num': count,
+        'name': name_receive,
+        'url': url_receive,
+        'choice': choice_receive,
+        'comment': comment_receive
+    }
+    db.food.insert_one(doc)
+
+    return jsonify({'result':'success', 'msg': '등록 완료!'})
+
+@app.route("/food", methods=["GET"])
+def food_get():
+    food_list = list(db.food.find({}, {'_id': False}))
+    return jsonify({'foods': food_list})
+
 #댓글 올리기 서버
 @app.route('/food/comment', methods=["POST"])
 def comment_post():
